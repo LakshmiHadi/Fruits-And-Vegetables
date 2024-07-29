fd28ff6fba07372ab4d6019c5c636b2b4a693677
Lakshmi Hadi <149565944+LakshmiHadi@users.noreply.github.com>
2024-07-29 20:27:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,4 +13,10 @@ let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
   menu.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+  menu.classList.remove('bx-x');
+  navbar.classList.remove('active');
 }
